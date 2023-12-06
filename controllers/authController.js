1b5c50a686ba8e6cfdfa1a94c3d200a1993c9b36
Yogesh <iamyogess@gmail.com>
2023-12-06 14:27:20 +0545

    reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,7 +76,96 @@ const login = async (req, res) => {
 };
 
 // change password by logged in user
+const changePassword = async (req, res) => {
+  const { newPassword, confirmNewPassword, email, currentPassword } = req.body;
+  try {
+    const user = await AuthModel.findOne({ email: email });
+    const oldPasswordDb = user.password;
+    if (newPassword && confirmNewPassword && oldPasswordDb && currentPassword) {
+      if (oldPasswordDb === previousPassword) {
+        if (newPassword === confirmNewPassword) {
+          const salt = bcrypt.genSalt(10);
+          const hashedPassword = bcrypt.hash(newPassword, salt);
+          //save new password
+          await AuthModel.findByIdAndUpdate(req.user._id, {
+            $set: { password: hashedPassword },
+          });
+          res.status(200).json({ message: "Password changed successfully!" });
+        } else {
+          res
+            .status(400)
+            .json({ message: "Password and Confirm Password does not match!" });
+        }
+      } else {
+        res.status(400).json({ message: "Current Password does not match!" });
+      }
+    } else {
+      res.status(400).json({ message: "All fields are required!" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const changePassword = async (req,res) => {
-  
-}
+//send reset password email
+const sendPasswordResetEmail = async (req, res) => {
+  const { email } = req.body;
+  if (email) {
+    const user = await AuthModel.findOne({ email: email });
+    if (user) {
+      const secret = user._id + process.env.SECRET_JWT_KEY;
+      const token = jwt.sign({ userID: user._id }, secret, {
+        expiresIn: "15m",
+      });
+      const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`;
+      let info = await transporter.sendMail({
+        from: process.env.EMAIL_FORM,
+        to: user.email,
+        subject: "Hello this is email from Nodemailer",
+        html: `<a href=${link}>Click Here</a> to Reset Your Password`,
+      });
+      res.status(200).json({
+        status: "Success",
+        message: "Password Reset Email Sent! Please Check Your Email.",
+      });
+    } else {
+      res.status(404).json({ status: "Failed", message: "User not found!" });
+    }
+  } else {
+    res
+      .status(401)
+      .json({ status: "Failed!", message: "Email Field is Required!" });
+  }
+};
+
+//reset password
+
+const resetPassword = async (req, res) => {
+  const { password, confirmPassword } = req.body;
+  const { id, token } = req.params;
+  const user = await UserModel.findById(id);
+  const new_secret = user._id + process.env.SECRET_JWT_KEY;
+  try {
+    jwt.verify(token, new_secret);
+    if (password && confirmPassword) {
+      if (password !== confirmPassword) {
+        res.send({
+          status: "failed",
+          message: "New Password and Confirm New Password doesn't match",
+        });
+      } else {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        await UserModel.findByIdAndUpdate(user._id, {
+          $set: { password: hashedPassword },
+        });
+        res.send({ status: "success", message: "Password Reset Successfully" });
+      }
+    } else {
+      res.send({ status: "failed", message: "All Fields are Required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ status: "failed", message: "Invalid Token" });
+  }
+};
